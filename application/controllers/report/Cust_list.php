a5edd1fbdb253cd74ca3af863411be01af920750
Darsono <darsono72@gmail.com>
2021-07-11 18:27:32 +0700

    update

--- application/controllers/report/Cust_list.php
+++ application/controllers/report/Cust_list.php
@@ -18,64 +18,75 @@ class Cust_list extends CI_Controller {
 		$html='';
 
 		$html.='<table id="example" class="table table-striped table-bordered" style="width:100%">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Customer</th>
-                <th>Address</th>
-                <th>Phone</th>
-                <th>Note</th>
-            </tr>
-        </thead>
-        <tbody>';
+		<thead>
+		<tr>
+		<th>#</th>
+		<th>Customer</th>
+		<th>Address</th>
+		<th>Phone</th>
+		<th>Note</th>
+		</tr>
+		</thead>
+		<tbody>';
 
-        $this->db->from('tbl_customer');
-        $qry=$this->db->get();
+		$this->db->from('tbl_customer');
+		$qry=$this->db->get();
 
-        $i=1;
-        foreach($qry->result() as $rows){
-	        $html.='    <tr>';
-	        $html.='        <td>'.$i.'</td>';
-	        $html.='        <td>'.$rows->cust_name.'</td>';
-	        $html.='        <td>'.$rows->cust_address.'</td>';
-	        $html.='        <td>'.$rows->cust_phone.'</td>';
-	        $html.='        <td></td>';
-	        $html.='    </tr>';  
-	        $i++;      	
-        }
+		$i=1;
+		foreach($qry->result() as $rows){
+			$html.='<tr>';
+			$html.='	<td>'.$i.'</td>';
+			$html.='    <td id="cust_name'.$i.'">'.$rows->cust_name.'</td>';
+			$html.='    <td id="cust_address'.$i.'">'.$rows->cust_address.'</td>';
+			$html.='    <td id="cust_phone'.$i.'">'.$rows->cust_phone.'</td>';
+			$html.='    <td><span class="btn btn-sm btn-primary" id="btn_edit'.$i.'" onclick="edit_cust('.$i.','.$rows->cust_id.')">Edit</span></td>';
+			$html.='</tr>';  
+			$i++;      	
+		}
 
-        $html.='</tbody>';
-        $html.='<tfoot>';
-        $html.='    <tr>';
-        $html.='        <th>#</th>';
-        $html.='        <th>Customer</th>';
-        $html.='        <th>Address</th>';
-        $html.='        <th>Phone</th>';
-        $html.='        <th>Note</th>';
-        $html.='    </tr>';
-        $html.='</tfoot>';
-    	$html.='</table>'; 
+		$html.='</tbody>';
+		$html.='<tfoot>';
+		$html.='    <tr>';
+		$html.='        <th>#</th>';
+		$html.='        <th>Customer</th>';
+		$html.='        <th>Address</th>';
+		$html.='        <th>Phone</th>';
+		$html.='        <th>Note</th>';
+		$html.='    </tr>';
+		$html.='</tfoot>';
+		$html.='</table>'; 
 
 
-    	echo $html;
+		echo $html;
 
+	}
+
+	function edit_data(){
+		$data=[
+			'cust_name'=>$this->input->post('cust_name'),
+			'cust_address'=>$this->input->post('cust_address'),
+			'cust_phone'=>$this->input->post('cust_phone') 
+		];
+
+		$this->db->where('cust_id',$this->input->post('cust_id'));
+		$this->db->update('tbl_customer', $data);
 	}
 
 	function copy_data(){
 		$this->db->from('tbl_customer');
-        $qry=$this->db->get();
+		$qry=$this->db->get();
 
-        $i=1;
-        foreach($qry->result() as $rows){
-	        $data=[
-	        	'cust_name'=>$rows->cust_name.' - '.rand(),
-	        	'cust_address'=>$rows->cust_address.' - '.rand(),
-	        	'cust_phone'=>rand()
-	        ];
- 
- 			$this->db->insert('tbl_customer',$data);
-	        $i++;      	
-        }
+		$i=1;
+		foreach($qry->result() as $rows){
+			$data=[
+				'cust_name'=>$rows->cust_name.' - '.rand(),
+				'cust_address'=>$rows->cust_address.' - '.rand(),
+				'cust_phone'=>rand()
+			];
+			
+			$this->db->insert('tbl_customer',$data);
+			$i++;      	
+		}
 	}
 
 }
--- assets/js/report/cust_list.js
+++ assets/js/report/cust_list.js
@@ -11,3 +11,34 @@ function get_data() {
 
     });
 }
+
+function edit_cust(i, cust_id) {
+    $('#my_modal').modal('show');
+    // console.log($('#cust_name' + i).html());
+
+    let body = '<div class="row">';
+    body += '<div class="col-sm-4"><input type="text" class="form-control" value="' + $('#cust_name' + i).html() + '" id="cust_name"></div>';
+    body += '<div class="col-sm-5"><input type="text" class="form-control" value="' + $('#cust_address' + i).html() + '" id="cust_address"></div>';
+    body += '<div class="col-sm-3"><input type="text" class="form-control" value="' + $('#cust_phone' + i).html() + '" id="cust_phone"></div>';
+    body += '</div>';
+    body += '<div class="row">';
+    body += '<div class="col-sm-3"><input type="hidden" class="form-control" value="' + cust_id + '" id="cust_id"></div>';
+    body += '</div>';
+
+    $('#my_modal').ready(function() {
+        $('#modal_title').html('EDIT CUSTOMER');
+        $('#modal_body').html(body);
+    })
+}
+
+$('#btn_save_modal').click(function() {
+    $.post(url + 'report/cust_list/edit_data', {
+        cust_name: $('#cust_name').val(),
+        cust_address: $('#cust_address').val(),
+        cust_phone: $('#cust_phone').val(),
+        cust_id: $('#cust_id').val()
+    }, function(result) {
+        $('#my_modal').modal('show');
+        get_data();
+    });
+})
